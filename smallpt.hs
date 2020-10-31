e1819a6c0420d0083071d9df25b7337d8ee7bae0
davean <oss@xkcd.com>
2020-10-31 22:43:11 +0000

    Use a pattern synonym to unpack Refl in Sphere.
    While we weren't able to unpack Refl in the last step because it
    is a sum type, we can if we use a trick modeled after an C Enum.
    The use of COMPLETE is unsafe here because other values could be
    constructed. For this small example though we rely on the fact
    that we create values of Refl via the patterns.

--- smallpt.hs
+++ smallpt.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE PatternSynonyms #-}
 module Main (main) where
 import qualified Data.Vector.Mutable as VM
 import Data.List (foldl')
@@ -30,10 +31,15 @@ maxv (Vec a b c) = maximum [a,b,c]
 
 data Ray = Ray {-# UNPACK #-} !Vec {-# UNPACK #-} !Vec -- origin, direction
 
-data Refl = DIFF | SPEC | REFR -- material types, used in radiance
+newtype Refl = Refl Int  -- material types, used in radiance
+pattern DIFF,SPEC,REFR :: Refl
+pattern DIFF = Refl 0
+pattern SPEC = Refl 1
+pattern REFR = Refl 2
+{-# COMPLETE DIFF, SPEC, REFR #-}
 
 -- radius, position, emission, color, reflection
-data Sphere = Sphere {-# UNPACK #-} !Double {-# UNPACK #-} !Vec {-# UNPACK #-} !Vec {-# UNPACK #-} !Vec !Refl
+data Sphere = Sphere {-# UNPACK #-} !Double {-# UNPACK #-} !Vec {-# UNPACK #-} !Vec {-# UNPACK #-} !Vec {-# UNPACK #-} !Refl
 
 intersect :: Ray -> Sphere -> Maybe Double
 intersect (Ray o d) (Sphere r p _e _c _refl) =
